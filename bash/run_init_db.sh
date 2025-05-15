ed27040c60f813e631761b42d1d1f0ffb8379b6a
gorbunovvitaly <vs.gorbunov@outlook.com>
2025-05-15 20:50:46 +0400

    Добавлена проверка версии docker

--- bash/run_init_db.sh
+++ bash/run_init_db.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+if ! [ -x "$(command -v docker)" ]; then
+  echo 'Error: Docker is not installed.' >&2
+  exit 1
+fi
+
+# Проверяем, установлен ли Docker Compose
+if ! [ -x "$(command -v docker-compose)" ]; then
+  echo 'Error: Docker Compose is not installed.' >&2
+  exit 1
+fi
